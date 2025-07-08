8b0fca1a0a92b94e356eab2074b8eaf5da18bf98
eddgerig <edgareduardogerig@gmail.com>
2025-07-07 21:29:56 -0400

    configuracion inicial

---
+++ src/App.tsx
@@ -0,0 +1,48 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Button from '@mui/material/Button'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+        <h1 className="text-3xl font-bold underline text-red-600">
+    Hello world!
+
+  </h1>
+        {/* Prueba de Tailwind */}
+      <div className="bg-blue-100 text-blue-800 p-4 rounded mb-4">
+        Este div usa estilos de Tailwind CSS
+      </div>
+      {/* Prueba de Material UI */}
+      <Button variant="contained" color="primary">
+        Botón de Material UI
+      </Button>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
