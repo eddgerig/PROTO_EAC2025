93f42b62c5408f26eae679bbfa85b1fb89580e15
eddgerig <edgareduardogerig@gmail.com>
2025-07-07 21:33:30 -0400

    Init home

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Button from '@mui/material/Button'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -17,19 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
-        <h1 className="text-3xl font-bold underline text-red-600">
-    Hello world!
-
-  </h1>
-        {/* Prueba de Tailwind */}
-      <div className="bg-blue-100 text-blue-800 p-4 rounded mb-4">
-        Este div usa estilos de Tailwind CSS
-      </div>
-      {/* Prueba de Material UI */}
-      <Button variant="contained" color="primary">
-        Botón de Material UI
-      </Button>
+      <h1>HOME</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
